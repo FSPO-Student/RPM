f28d20517fa24b97586e890ccd7d13037ec3ba9c
Don2Quixote <idonquixote@yandex.ru>
2020-12-14 19:11:12 +0300

    Changed int to float and added dividing

--- LR2/main.cpp
+++ LR2/main.cpp
@@ -14,7 +14,7 @@ std::stack<std::string> reverseStack(std::stack<std::string> stack) {
     return newStack;
 }
 
-int calculate(std::stack<std::string> stack) {
+float calculate(std::stack<std::string> stack) {
     stack = reverseStack(stack);
     std::stack<std::string> subStack; 
     
@@ -22,42 +22,73 @@ int calculate(std::stack<std::string> stack) {
         std::string value = stack.top();
         stack.pop();
         if (value == "+") {
-            int value2 = std::stoi(subStack.top());
+            float value2 = std::stof(subStack.top());
             subStack.pop();
-            int value1 = std::stoi(subStack.top());
+            float value1 = std::stof(subStack.top());
             subStack.pop();
+            // std::cout << value1 << "+" << value2 << std::endl;
             subStack.push(std::to_string(value1 + value2));
         } else if (value == "-") {
-            int value2 = std::stoi(subStack.top());
+            float value2 = std::stof(subStack.top());
             subStack.pop();
-            int value1 = std::stoi(subStack.top());
+            float value1 = std::stof(subStack.top());
             subStack.pop();
+            // std::cout << value1 << "-" << value2 << std::endl;
             subStack.push(std::to_string(value1 - value2));
         } else if (value == "*") {
-            int value2 = std::stoi(subStack.top());
+            float value2 = std::stof(subStack.top());
             subStack.pop();
-            int value1 = std::stoi(subStack.top());
+            float value1 = std::stof(subStack.top());
             subStack.pop();
+            // std::cout << value1 << "*" << value2 << std::endl;
             subStack.push(std::to_string(value1 * value2));
+        } else if (value == "/") {
+            float value2 = std::stof(subStack.top());
+            subStack.pop();
+            float value1 = std::stof(subStack.top());
+            subStack.pop();
+            // std::cout << value1 << "/" << value2 << std::endl;
+            if (value2 == 0) {
+                throw "Not cool dividing";
+            }
+            subStack.push(std::to_string(value1 / value2));
         } else {
             subStack.push(value);
         }
     }
 
-    return std::stoi(subStack.top());
+    return std::stof(subStack.top());
 }
 
 int main() {
-    std::stack<std::string> stack;
+    std::stack<std::string> stack1;
     // 11 * (7 + 4) - 74
-    stack.push("11");
-    stack.push("7");
-    stack.push("4");
-    stack.push("+");
-    stack.push("*");
-    stack.push("74");
-    stack.push("-");
+    stack1.push("11");
+    stack1.push("7");
+    stack1.push("4");
+    stack1.push("+");
+    stack1.push("*");
+    stack1.push("74");
+    stack1.push("-");
 
-    std::cout << calculate(stack) << std::endl;
+    std::cout << calculate(stack1) << std::endl;
+
+    std::stack<std::string> stack2;
+    // (7 + 2) / (3 * 3) / 0
+    stack2.push("7");
+    stack2.push("2");
+    stack2.push("+");
+    stack2.push("3");
+    stack2.push("3");
+    stack2.push("*");
+    stack2.push("/");
+    stack2.push("0");
+    stack2.push("/");
+    try {
+        std::cout << calculate(stack2) << std::endl;
+    } catch (char const *err) {
+        std::cout << "Error: " << err << std::endl;
+    }
+
     return 0;
 }
